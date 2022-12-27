ad2e4aa5832167fe1599734bbdadac7ec6a2f4c9
Marius Winkler <mariusjwinkler@gmail.com>
2022-12-27 18:15:53 +0100

    Revert "v1.3 smart execution - checks for upstream changes"
    This reverts commit 75fe9a0017a1d37ec3bf4c63941c4c577a1ce7f3.

--- update.sh
+++ update.sh
@@ -10,23 +10,7 @@ fi
 
 echo running the program for $key
 
-skip=false
-local=$(git -C pgdp2223-tests rev-parse HEAD)
-git fetch
-upstream=$(git -C pgdp2223-tests rev-parse @{u})
-if [ $local == $upstream ]; then
-	read -n 1 -p "No update found. Do you want to continue anyway? [Y/N]" yn
-	case $yn in
-		[Yy] ) ;;
-		* ) skip=true;;
-	esac
-	echo
-fi
-
-if [ $skip == false ]; then
-	git -C pgdp2223-tests pull
-	java -jar moveUtility.jar $key
-fi
+git -C pgdp2223-tests pull
+java -jar moveUtility.jar $key
 
 read -s -n 1 -p "Press any key to continue . . ."
-echo
